3e44eb7595483ff71ba9e90ee254f3718f4bebd9
thulasidharan96 <thulasidharankvp@gmail.com>
2025-06-23 15:15:15 +0530

    Seperated Btns

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,4 +1,5 @@
 import { useMetaMask } from "@/src/hooks/useMetaMask";
+import { Button } from "react-day-picker";
 
 export default function YourComponent() {
   const {
@@ -10,11 +11,6 @@ export default function YourComponent() {
     addNetwork,
     addBSCNetwork,
   } = useMetaMask();
-
-  const addMultiChain = async () => {
-    await addNetwork();
-    await addBSCNetwork();
-  };
 
   return (
     <div>
@@ -47,13 +43,20 @@ export default function YourComponent() {
           ? `Connected: ${account.slice(0, 6)}...${account.slice(-4)}`
           : "Connect Wallet"}
       </button> */}
-
-      <button
-        onClick={addMultiChain}
-        className="bg-[#F4B448] hover:bg-[#F4B448]/90 text-black font-semibold px-6 py-2 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed"
-      >
-        Add Areal Network
-      </button>
+      <div className="gap-4 flex flex-row">
+        <button
+          onClick={addNetwork}
+          className="bg-[#F4B448] hover:bg-[#F4B448]/90 text-black font-semibold px-6 py-2 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed"
+        >
+          Add Areal Network
+        </button>
+        <button
+          onClick={addBSCNetwork}
+          className="bg-[#F4B448] hover:bg-[#F4B448]/90 text-black font-semibold px-6 py-2 rounded-lg disabled:opacity-50 disabled:cursor-not-allowed"
+        >
+          Add BSTTEST Net
+        </button>
+      </div>
     </div>
   );
 }
