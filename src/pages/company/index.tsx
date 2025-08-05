2ebccfe1ca2950c57498f8799f1b88aa676f647b
thulasidharan96 <thulasidharankvp@gmail.com>
2025-08-05 12:01:47 +0530

    Board Member update

--- src/pages/company/index.tsx
+++ src/pages/company/index.tsx
@@ -74,7 +74,7 @@ const teamMembers: TeamMember[] = [
 
 const advisors: AdvisorMember[] = [
   {
-    name: "Mr. Adrian Oleary",
+    name: "Mr. Adrian O'leary",
     role: "Head of Product Quality @ Light & Wonder",
     image: "/team/adrian.jpeg",
     linkedin: "https://www.linkedin.com/in/adrianoleary/",
